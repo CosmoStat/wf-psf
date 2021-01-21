3597aa5acfbddbac8fead35740df840a24e8dac6
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-01-21 12:57:37 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wf-psf',
+      version='0.0.1',
+      description='Differentiable wavefront-based PSF modelling',
+      url='https://github.com/tobias-liaudat/wf-psf',
+      author='Tobias Liaudat',
+      author_email='tobias.liaudat@cea.fr',
+      license='MIT',
+      packages=['wf_psf'],
+      zip_safe=False)
