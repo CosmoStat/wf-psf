8b3787f4b71a6df6eac35c1e9c86ddada6469370
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-01-29 14:54:28 +0100

    Added new semi-parametric PSF model
    Also added docstrings

--- wf_psf/tf_psf_field.py
+++ wf_psf/tf_psf_field.py
@@ -4,12 +4,31 @@ from wf_psf.tf_layers import TF_poly_Z_field, TF_zernike_OPD, TF_batch_poly_PSF
 
 
 class TF_PSF_field_model(tf.keras.Model):
-    """ PSF field forward model!
+    """ Parametric PSF field model!
 
-    Fully parametric model based on the Zernike polynomial basis. The
+    Fully parametric model based on the Zernike polynomial basis.
 
     Parameters
     ----------
+    zernike_maps: Tensor(n_batch, opd_dim, opd_dim)
+        Zernike polynomial maps.
+    obscurations: Tensor(opd_dim, opd_dim)
+        Predefined obscurations of the phase.
+    batch_size: int
+        Batch size
+    output_dim: int
+        Output dimension of the PSF stamps.
+    n_zernikes: int
+        Order of the Zernike polynomial for the parametric model.
+    d_max: int
+        Maximum degree of the polynomial for the Zernike coefficient variations.
+    x_lims: [float, float]
+        Limits for the x coordinate of the PSF field.
+    y_lims: [float, float]
+        Limits for the x coordinate of the PSF field.
+    coeff_mat: Tensor or None
+        Initialization of the coefficient matrix defining the parametric psf
+        field model.
 
     """
     def __init__(self, zernike_maps, obscurations, batch_size,
@@ -111,3 +130,121 @@ def build_PSF_model(model_inst, optimizer=None, loss=None,
                        run_eagerly=False)
 
     return model_inst
+
+
+class TF_SemiParam_field(tf.keras.Model):
+    """ PSF field forward model!
+
+    Semi parametric model based on the Zernike polynomial basis. The
+
+    Parameters
+    ----------
+    zernike_maps: Tensor(n_batch, opd_dim, opd_dim)
+        Zernike polynomial maps.
+    obscurations: Tensor(opd_dim, opd_dim)
+        Predefined obscurations of the phase.
+    batch_size: int
+        Batch size
+    d_max_nonparam: int
+        Maximum degree of the polynomial for the non-parametric variations.
+    output_dim: int
+        Output dimension of the PSF stamps.
+    n_zernikes: int
+        Order of the Zernike polynomial for the parametric model.
+    d_max: int
+        Maximum degree of the polynomial for the Zernike coefficient variations.
+    x_lims: [float, float]
+        Limits for the x coordinate of the PSF field.
+    y_lims: [float, float]
+        Limits for the x coordinate of the PSF field.
+    coeff_mat: Tensor or None
+        Initialization of the coefficient matrix defining the parametric psf
+        field model.
+
+    """
+    def __init__(self, zernike_maps, obscurations, batch_size,
+        d_max_nonparam=3,
+        output_dim=64, n_zernikes=45, d_max=2, x_lims=[0, 1e3], y_lims=[0, 1e3],
+        coeff_mat=None, name='TF_SemiParam_field'):
+        super(TF_PSF_field_model, self).__init__()
+
+        # Inputs: TF_poly_Z_field
+        self.n_zernikes = n_zernikes
+        self.d_max = d_max
+        self.x_lims = x_lims
+        self.y_lims = y_lims
+
+        # Inputs: TF_NP_poly_OPD
+        self.d_max_nonparam = d_max_nonparam
+        self.opd_dim = tf.shape(zernike_maps)[1].numpy()
+
+        # Inputs: TF_zernike_OPD
+        # They are not stored as they are memory-heavy
+        # zernike_maps =[]
+
+        # Inputs: TF_batch_poly_PSF
+        self.batch_size = batch_size
+        self.obscurations = obscurations
+        self.output_dim = output_dim
+        self.psf_batch = tf.zeros(
+            (self.batch_size, self.output_dim, self.output_dim),
+            dtype=tf.float32)
+
+
+        # Initialize the first layer
+        self.tf_poly_Z_field = TF_poly_Z_field(x_lims=self.x_lims,
+                                                y_lims=self.y_lims,
+                                                n_zernikes=self.n_zernikes,
+                                                d_max=self.d_max)
+
+        # Initialize the zernike to OPD layer
+        self.tf_zernike_OPD = TF_zernike_OPD(zernike_maps=zernike_maps)
+
+        # Initialize the non-parametric layer
+        self.tf_np_poly_opd = TF_NP_poly_OPD(x_lims=self.x_lims,
+                                             y_lims=self.y_lims,
+                                             d_max=self.d_max_nonparam,
+                                             opd_dim=self.opd_dim)
+
+        # Initialize the batch opd to batch polychromatic PSF layer
+        self.tf_batch_poly_PSF = TF_batch_poly_PSF(obscurations=self.obscurations,
+                                                    psf_batch=self.psf_batch,
+                                                    output_dim=self.output_dim)
+
+        # Initialize the model parameters with non-default value
+        if coeff_mat is not None:
+            self.assign_coeff_matrix(coeff_mat)
+
+    def get_coeff_matrix(self):
+        """ Get coefficient matrix."""
+        return self.tf_poly_Z_field.get_coeff_matrix()
+
+    def assign_coeff_matrix(self, coeff_mat):
+        """ Assign coefficient matrix."""
+        self.tf_poly_Z_field.assign_coeff_matrix(coeff_mat)
+
+    def call(self, inputs):
+        """Define the PSF field forward model.
+
+        [1] From positions to Zernike coefficients
+        [2] From Zernike coefficients to OPD maps
+        [3] From OPD maps and SED info to polychromatic PSFs
+
+        OPD: Optical Path Differences
+        """
+        # Unpack inputs
+        input_positions = inputs[0]
+        packed_SEDs = inputs[1]
+
+        # Forward model
+        # Calculate parametric part
+        zernike_coeffs = self.tf_poly_Z_field(input_positions)
+        param_opd_maps = self.tf_zernike_OPD(zernike_coeffs)
+        # Calculate the non parametric part
+        nonparam_opd_maps =  self.tf_np_poly_opd(input_positions)
+        # Add the estimations
+        opd_maps = tf.math.add(param_opd_maps, nonparam_opd_maps)
+        # Compute the polychromatic PSFs
+        poly_psfs = self.tf_batch_poly_PSF([opd_maps, packed_SEDs])
+
+        return poly_psfs
