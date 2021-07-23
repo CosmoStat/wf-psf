3072dfd132e64f1bc07b127210142d0ac4a2eb06
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-07-23 18:15:55 +0200

    Minor update of GPU requirement

--- jz-submissions/jobs/training_poly_1000.sh
+++ jz-submissions/jobs/training_poly_1000.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
 #SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
 #SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
+#SBATCH -C v100-32g
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
 #SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
