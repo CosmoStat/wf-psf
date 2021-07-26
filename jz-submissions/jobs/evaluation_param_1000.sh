7de081b1bb398e32b6f3f1d6a64c6b882d25312a
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-07-26 14:19:22 +0200

    Adding remaining evaluation scripts for he param model

---
+++ jz-submissions/jobs/evaluation_param_1000.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=1k_param_eval      # nom du job
+##SBATCH --partition=gpu_p2          # de-commente pour la partition gpu_p2
+#SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:1                 # nombre de GPU par n?~Sud (max 8 avec gpu_p2)
+#SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
+# /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
+#SBATCH --hint=nomultithread         # hyperthreading desactive
+#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --output=1k_param_eval%j.out  # nom du fichier de sortie
+#SBATCH --error=1k_param_eval%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH -A xdy@gpu                   # specify the project
+##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+
+# nettoyage des modules charges en interactif et herites par defaut
+module purge
+
+# chargement des modules
+module load tensorflow-gpu/py3/2.4.1
+
+# echo des commandes lancees
+set -x
+
+cd $WORK/repo/wf-psf/jz-submissions/slurm-logs/
+
+srun python ./../scripts/evaluation_param_1000.py
