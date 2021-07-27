5e6ac3b08039fa309eec6596c6ee8e9af32f6260
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-07-28 00:30:28 +0200

    Testing cli

--- jz-submissions/jobs-test/generic_cli_train_eval_v2.sh
+++ jz-submissions/jobs-test/generic_cli_train_eval_v2.sh
@@ -8,11 +8,11 @@
 ##SBATCH -C v100-32g 
 # /!\ Attention, "multithread" fait reference a l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
-#SBATCH --time=20:00:00              # temps d'execution maximum demande (HH:MM:SS)
+#SBATCH --time=00:05:00              # temps d'execution maximum demande (HH:MM:SS)
 #SBATCH --output=2k_poly_tr_ev_click%j.out  # nom du fichier de sortie
 #SBATCH --error=2k_poly_tr_ev_click%j.err   # nom du fichier d'erreur (ici commun avec la sortie)
 #SBATCH -A xdy@gpu                   # specify the project
-##SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
+#SBATCH --qos=qos_gpu-dev            # using the dev queue, as this is only for profiling
 
 # nettoyage des modules charges en interactif et herites par defaut
 module purge
