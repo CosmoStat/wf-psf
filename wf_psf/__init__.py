cb379b3d5e8073774eaf1b586ef1c4fe6d835a04
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-07-28 14:26:26 +0200

    Adding alternative psf models module. New poly mode l2 loss on the OPD

--- wf_psf/__init__.py
+++ wf_psf/__init__.py
@@ -8,12 +8,14 @@ from .tf_layers import *
 from .tf_modules import *
 from .tf_psf_field import *
 from .tf_mccd_psf_field import *
+from .tf_alt_psf_models import *
 
 from .utils import *
 from .metrics import *
 from .train_utils import *
 
 __all__ = []  # List of submodules
-__all__ += [SimPSFToolkit, GenPolyFieldPSF]
-__all__ += [tf_layers, tf_modules, tf_psf_field, tf_mccd_psf_field]
-__all__ += [graph_utils, utils, metrics, train_utils]
+__all__ += ['SimPSFToolkit', 'GenPolyFieldPSF']
+__all__ += ['tf_layers', 'tf_modules', 'tf_psf_field', 'tf_mccd_psf_field']
+__all__ += ['graph_utils', 'utils', 'metrics', 'train_utils']
+__all__ += ['tf_alt_psf_models']
