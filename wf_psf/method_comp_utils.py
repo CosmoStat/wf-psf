763acd0b120bc16ad649f84b8eae1757f0b38f94
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-08-31 16:06:19 +0200

    Added shape_pix metric calculation and ground truth model management

--- wf_psf/method_comp_utils.py
+++ wf_psf/method_comp_utils.py
@@ -4,6 +4,10 @@ import numpy as np
 from astropy.io import fits
 import mccd
 import galsim as gs
+import tensorflow as tf
+from wf_psf import utils as utils
+from wf_psf import SimPSFToolkit as SimPSFToolkit
+from wf_psf import tf_psf_field as psf_field
 
 def interpsfex(dotpsfpath, pos):
     """Use PSFEx generated model to perform spatial PSF interpolation.
@@ -110,9 +114,8 @@ def validation_stars(dotpsfpath, test_stars, test_pos, psf_size=1.25):
             
     return interp_psfs
 
-# Measure shapes
-def calc_shapes(matched_psfs, test_stars):
 
+def calc_shapes(matched_psfs, test_stars):
     # Measure shapes of the reconstructions
     pred_moments = [gs.hsm.FindAdaptiveMom(gs.Image(_pred), strict=False) for _pred in matched_psfs]
 
@@ -145,3 +148,163 @@ def calc_shapes(matched_psfs, test_stars):
     
     return (pred_e1_HSM, pred_e2_HSM, pred_R2_HSM), (GT_pred_e1_HSM, GT_pred_e2_HSM, GT_pred_R2_HSM)
 
+
+def gen_GT_wf_model(test_wf_file_path, pred_output_Q=1, pred_output_dim=64):
+    r""" Generate the ground truth model and output test PSF ar required resolution. 
+
+    If `pred_output_Q=1` the resolution will be 3 times the one of Euclid.
+    """
+    # Load dataset
+    wf_test_dataset = np.load(test_wf_file_path, allow_pickle=True)[()]
+    
+    # Extract parameters from the wf test dataset
+    wf_test_params = wf_test_dataset['parameters']
+    wf_test_C_poly = wf_test_dataset['C_poly']
+    wf_test_pos = wf_test_dataset['positions']
+    tf_test_pos = tf.convert_to_tensor(wf_test_pos, dtype=tf.float32)
+    wf_test_SEDs = wf_test_dataset['SEDs']
+
+    # Generate GT model
+    batch_size = 16
+
+    # Generate Zernike maps
+    zernikes = utils.zernike_generator(
+        n_zernikes=wf_test_params['max_order'],
+        wfe_dim=wf_test_params['pupil_diameter']
+    )
+
+    ## Generate initializations
+    # Prepare np input
+    simPSF_np = SimPSFToolkit(
+        zernikes,
+        max_order=wf_test_params['max_order'],
+        pupil_diameter=wf_test_params['pupil_diameter'],
+        output_dim=wf_test_params['output_dim'],
+        oversampling_rate=wf_test_params['oversampling_rate'],
+        output_Q=wf_test_params['output_Q']
+    )
+    simPSF_np.gen_random_Z_coeffs(max_order=wf_test_params['max_order'])
+    z_coeffs = simPSF_np.normalize_zernikes(simPSF_np.get_z_coeffs(), simPSF_np.max_wfe_rms)
+    simPSF_np.set_z_coeffs(z_coeffs)
+    simPSF_np.generate_mono_PSF(lambda_obs=0.7, regen_sample=False)
+    # Obscurations
+    obscurations = simPSF_np.generate_pupil_obscurations(
+        N_pix=wf_test_params['pupil_diameter'],
+        N_filter=wf_test_params['LP_filter_length']
+    )
+    tf_obscurations = tf.convert_to_tensor(obscurations, dtype=tf.complex64)
+
+
+    ## Prepare ground truth model
+    # Now Zernike's as cubes
+    np_zernike_cube = np.zeros((len(zernikes), zernikes[0].shape[0], zernikes[0].shape[1]))
+    for it in range(len(zernikes)):
+        np_zernike_cube[it,:,:] = zernikes[it]
+
+    np_zernike_cube[np.isnan(np_zernike_cube)] = 0
+    tf_zernike_cube = tf.convert_to_tensor(np_zernike_cube, dtype=tf.float32)
+
+    # Initialize the model
+    GT_tf_semiparam_field = psf_field.TF_SemiParam_field(
+        zernike_maps=tf_zernike_cube,
+        obscurations=tf_obscurations,
+        batch_size=batch_size,
+        output_Q=wf_test_params['output_Q'],
+        d_max_nonparam=2,
+        output_dim=wf_test_params['output_dim'],
+        n_zernikes=wf_test_params['max_order'],
+        d_max=wf_test_params['d_max'],
+        x_lims=wf_test_params['x_lims'],
+        y_lims=wf_test_params['y_lims']
+    )
+
+    # For the Ground truth model
+    GT_tf_semiparam_field.tf_poly_Z_field.assign_coeff_matrix(wf_test_C_poly)
+    _ = GT_tf_semiparam_field.tf_np_poly_opd.alpha_mat.assign(
+        tf.zeros_like(GT_tf_semiparam_field.tf_np_poly_opd.alpha_mat)
+    )
+
+    # Set required output_Q
+
+    GT_tf_semiparam_field.set_output_Q(output_Q=pred_output_Q, output_dim=pred_output_dim)
+
+    GT_tf_semiparam_field = psf_field.build_PSF_model(GT_tf_semiparam_field)
+
+    packed_SED_data = [
+        utils.generate_packed_elems(
+            _sed,
+            simPSF_np,
+            n_bins=wf_test_params['n_bins']
+        )
+        for _sed in wf_test_SEDs
+    ]
+
+    # Prepare inputs
+    tf_packed_SED_data = tf.convert_to_tensor(packed_SED_data, dtype=tf.float32)
+    tf_packed_SED_data = tf.transpose(tf_packed_SED_data, perm=[0, 2, 1])
+    pred_inputs = [tf_test_pos , tf_packed_SED_data]
+
+    # Ground Truth model
+    GT_predictions = GT_tf_semiparam_field.predict(x=pred_inputs, batch_size=batch_size)
+
+    return GT_predictions, wf_test_pos
+
+def shape_pix_metrics(exp_psfs, exp_stars):
+
+    # Calculate residuals
+    residuals = np.sqrt(np.mean((exp_psfs - exp_stars)**2, axis=(1,2)))
+    GT_star_mean = np.sqrt(np.mean((exp_stars)**2, axis=(1,2)))
+
+    # RMSE calculations
+    rmse = np.mean(residuals)
+    rel_rmse = 100. * np.mean(residuals/GT_star_mean)
+
+    # STD calculations
+    pix_rmse_std = np.std(residuals)
+    rel_pix_rmse_std = 100. * np.std(residuals/GT_star_mean)
+
+    ## Shape error
+    # Calculate shapes
+    psf_shapes, star_shapes = calc_shapes(exp_psfs, exp_stars)
+    # Extract results
+    pred_e1_HSM, pred_e2_HSM, pred_R2_HSM = psf_shapes[0], psf_shapes[1], psf_shapes[2]
+    GT_pred_e1_HSM, GT_pred_e2_HSM, GT_pred_R2_HSM = star_shapes[0], star_shapes[1], star_shapes[2]
+
+    # e1
+    e1_res = GT_pred_e1_HSM - pred_e1_HSM
+    e1_res_rel = (GT_pred_e1_HSM - pred_e1_HSM) / GT_pred_e1_HSM
+
+    rmse_e1 = np.sqrt(np.mean(e1_res**2))
+    rel_rmse_e1 = 100.* np.sqrt(np.mean(e1_res_rel**2))
+    std_rmse_e1 = np.std(e1_res)
+    std_rel_rmse_e1 = 100. * np.std(e1_res_rel)
+
+    # e2
+    e2_res = GT_pred_e2_HSM - pred_e2_HSM
+    e2_res_rel = (GT_pred_e2_HSM - pred_e2_HSM) / GT_pred_e2_HSM
+
+    rmse_e2 = np.sqrt(np.mean(e2_res**2))
+    rel_rmse_e2 = 100.* np.sqrt(np.mean(e2_res_rel**2))
+    std_rmse_e2 = np.std(e2_res)
+    std_rel_rmse_e2 = 100. * np.std(e2_res_rel)
+
+    # R2
+    R2_res = GT_pred_R2_HSM - pred_R2_HSM
+
+    rmse_R2_meanR2 = np.sqrt(np.mean(R2_res**2))/np.mean(GT_pred_R2_HSM)
+    std_rmse_R2_meanR2 = np.std(R2_res/GT_pred_R2_HSM)
+
+    # Save the metrics
+    metrics_dict = {
+        'pix_rmse': rmse, 'pix_rel_rmse': rel_rmse,
+        'pix_rmse_std': pix_rmse_std, 'rel_pix_rmse_std': rel_pix_rmse_std,
+        'pred_e1_HSM': pred_e1_HSM, 'pred_e2_HSM': pred_e2_HSM, 'pred_R2_HSM': pred_R2_HSM,
+        'GT_pred_e1_HSM': GT_pred_e1_HSM, 'GT_ped_e2_HSM': GT_pred_e2_HSM, 'GT_pred_R2_HSM': GT_pred_R2_HSM,
+        'rmse_e1': rmse_e1, 'std_rmse_e1': std_rmse_e1,
+        'rel_rmse_e1': rel_rmse_e1, 'std_rel_rmse_e1': std_rel_rmse_e1,
+        'rmse_e2': rmse_e2, 'std_rmse_e2': std_rmse_e2,
+        'rel_rmse_e2': rel_rmse_e2, 'std_rel_rmse_e2': std_rel_rmse_e2,
+        'rmse_R2_meanR2': rmse_R2_meanR2, 'std_rmse_R2_meanR2': std_rmse_R2_meanR2,
+    }
+
+    return metrics_dict
