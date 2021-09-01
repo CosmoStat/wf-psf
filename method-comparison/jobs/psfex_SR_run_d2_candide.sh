7528420ccf94508f240a894f7a4a68040dd90842
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-01 16:51:15 +0200

    updating psfex SR jobs

--- method-comparison/jobs/psfex_SR_run_d2_candide.sh
+++ method-comparison/jobs/psfex_SR_run_d2_candide.sh
@@ -8,13 +8,13 @@
 #PBS -M tobias.liaudat@cea.fr
 #PBS -m ea
 # Set a name for the job
-#PBS -N psfex_SR_run
+#PBS -N psfex_SR_run_d2
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=99:00:00
 # Request number of cores
-#PBS -l nodes=n02:ppn=02
+#PBS -l nodes=n16:ppn=02
 
 # Activate conda environment
 # module load intelpython/3-2020.1
@@ -26,7 +26,7 @@ cd /home/tliaudat/github/wf-psf/
 
 python ./method-comparison/scripts/psfex_script_SR.py \
     --repo_base_path /home/tliaudat/github/wf-psf/ \
-    --saving_dir /n05data/tliaudat/wf_exps/outputs/psfex_SR/ \
+    --saving_dir /n05data/tliaudat/wf_exps/outputs/psfex_SR_d2/ \
     --dataset_path /n05data/tliaudat/wf_exps/datasets/psfex/ \
     --psfvar_degrees 2 \
     --psf_sampling 0.33 \
