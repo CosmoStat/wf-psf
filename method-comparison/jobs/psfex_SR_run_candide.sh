6b208b456fe4fdf18210f4aeed0c35f5c34ddd3c
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-01 09:08:01 +0200

    add psfex SR job for candide

---
+++ method-comparison/jobs/psfex_SR_run_candide.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+
+# Receive email when job finishes or aborts
+#PBS -M tobias.liaudat@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N psfex_SR_run
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=99:00:00
+# Request number of cores
+#PBS -l nodes=n16:ppn=02
+
+# Activate conda environment
+# module load intelpython/3-2020.1
+module load tensorflow/2.4
+module load intel/19.0/2
+source activate new_shapepipe
+
+cd /home/tliaudat/github/wf-psf/
+
+python ./method-comparison/scripts/psfex_script_SR.py \
+    --repo_base_path /home/tliaudat/github/wf-psf/ \
+    --saving_dir /n05data/tliaudat/wf_exps/outputs/psfex_SR/ \
+    --dataset_path /n05data/tliaudat/wf_exps/datasets/psfex/ \
+    --psfvar_degrees 2 \
+    --psf_sampling 0.33 \
+    --psf_size 64 \
+    --run_id psfex_SR_run_d2 \
+    --exec_path psfex \
+    --verbose 1 \
+
+# Return exit code
+exit 0
