700c336b6ace05ebe00215bc81c0361af8191d20
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-03 17:22:21 +0200

    Adding new data-gen with shifts jobs for candide

---
+++ method-comparison/jobs/rca_data_gen_shifts_candide.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+
+# Receive email when job finishes or aborts
+#PBS -M tobias.liaudat@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N rca_data_gen_shifts
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=99:00:00
+# Request number of cores
+#PBS -l nodes=n03:ppn=02
+
+# Activate conda environment
+# module load intelpython/3-2020.1
+module load tensorflow/2.4
+module load intel/19.0/2
+source activate new_shapepipe
+
+cd /home/tliaudat/github/wf-psf/
+
+python ./method-comparison/scripts/dataset-conversion-RCA-intrapixshifts.py \
+    --base_repo_path /home/tliaudat/github/wf-psf/ \
+    --rca_saving_path /n05data/tliaudat/wf_exps/datasets/rca_shifts/ \
+
+
+# Return exit code
+exit 0
