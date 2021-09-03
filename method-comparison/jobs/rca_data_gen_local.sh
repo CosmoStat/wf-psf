6d398f80fc7d15f6eb81605b0d8c90a554eca66c
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-03 17:11:32 +0200

    New local data-conversion jobs

---
+++ method-comparison/jobs/rca_data_gen_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/method-comparison/scripts/dataset-conversion-RCA-intrapixshifts.py \
+    --base_repo_path /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/ \
+    --rca_saving_path /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/method-comparison/compatible-datasets/rca_shifts/ \
