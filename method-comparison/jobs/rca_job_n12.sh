9caf110cbc3f6b98227d55666e9df3793e21c33e
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-07 09:23:17 +0200

    Update  nodes

--- method-comparison/jobs/rca_job_n12.sh
+++ method-comparison/jobs/rca_job_n12.sh
@@ -14,7 +14,7 @@
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=99:00:00
 # Request number of cores
-#PBS -l nodes=n03:ppn=02
+#PBS -l nodes=n02:ppn=02
 
 # Activate conda environment
 module load intelpython/3-2020.1
