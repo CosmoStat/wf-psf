61abf0199fd699baf7cea95d18b6dca1184129f7
Tobias Liaudat <tobiasliaudat@gmail.com>
2021-09-07 15:15:58 +0200

    new test job

--- method-comparison/jobs/test_rca_job.sh
+++ method-comparison/jobs/test_rca_job.sh
@@ -8,26 +8,33 @@
 #PBS -M tobias.liaudat@cea.fr
 #PBS -m ea
 # Set a name for the job
-#PBS -N rca_wf_exp_test
+#PBS -N test_rca
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=99:00:00
 # Request number of cores
-#PBS -l nodes=n03:ppn=04
+#PBS -l nodes=n05:ppn=04
 
 # Activate conda environment
 module load intelpython/3-2020.1
 module load intel/19.0/2
 source activate new_shapepipe
 
-cd /home/tliaudat/github/mccd_develop/mccd/wf_exps/log_files/
+cd /home/tliaudat/github/wf-psf/method-comparison/scripts/
 
-python ./../scripts/test_script.py \
-    --run_id rca_n9_up1_k3 \
-    --n_comp 9 \
-    --upfact 2 \
-    --ksig 2. \
+# Just 1 for OMP_NUM_THREADS for this Python script
+export OMP_NUM_THREADS=$NSLOTS
+# And let the low-level threading use all of the requested cores
+export OPENBLAS_NUM_THREADS=1
+
+python ./rca_script.py \
+    --run_id test_rca_shifts_n8_up1_k3 \
+    --n_comp 8 \
+    --upfact 1 \
+    --ksig 3. \
+    --saving_dir /n05data/tliaudat/wf_exps/outputs/rca_shifts/ \
+    --input_data_dir /n05data/tliaudat/wf_exps/datasets/rca_shifts/ \
 
 # Return exit code
 exit 0
