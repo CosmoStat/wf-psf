46caf8aea172a67348a60033d547fc77dcee5210
jeipollack <jpollacke@gmail.com>
2023-01-27 11:45:08 +0100

    Cleaned up syntax

--- wf_psf/read_config.py
+++ wf_psf/read_config.py
@@ -1,5 +1,5 @@
 """
-:file: wf_psf/train.py
+:file: wf_psf/read_config.py
 
 :date: 18/01/23
 :author: jpollack
@@ -8,7 +8,6 @@
 
 import yaml
 import pprint
-
 from types import SimpleNamespace
 import os
 
@@ -31,7 +30,7 @@ class RecursiveNamespace(SimpleNamespace):
                 setattr(self, key, list(map(self.map_entry, val)))
 
 
-def read_yaml(conf_file="config.yaml"):
+def read_yaml(conf_file):
     """A function to read YAML file."""
     with open(conf_file) as f:
         config = yaml.safe_load(f)
@@ -39,19 +38,17 @@ def read_yaml(conf_file="config.yaml"):
     return config
 
 
-def read_conf(conf_file="config.yaml"):
+def read_conf(conf_file):
     """A function to read a yaml conf file recursively."""
     with open(conf_file, "r") as f:
         my_conf = yaml.safe_load(f)
     return RecursiveNamespace(**my_conf)
 
 
-def read_stream(conf_file="configs.yaml"):
-    """A function to read multiple docs in a config."""
+def read_stream(conf_file):
+    """A function to read multiple docs in a yaml config."""
     stream = open(conf_file, "r")
     docs = yaml.load_all(stream, yaml.FullLoader)
-   # with open(conf_file, "r") as f:
-    #    docs = yaml.safe_load_all(f)
 
     for doc in docs:
         conf = RecursiveNamespace(**doc)
@@ -63,19 +60,20 @@ if __name__ == "__main__":
 
     # read the yaml config
     my_config = read_yaml(os.path.join(
-        workdir, "Projects/wf-psf/wf_psf/config/training_config.yaml"))
+        workdir, "Projects/wf-psf/config/training_config.yaml"))
 
     # prtty print my_config
     pprint.pprint(my_config)
 
     # prtty print env config
     wf_conf = read_conf(os.path.join(
-        workdir, "Projects/wf-psf/wf_psf/config/config.yaml"))
+        workdir, "Projects/wf-psf/config/training_config.yaml"))
     pprint.pprint(wf_conf)
 
     # prtty print multiple docs in config
     wf_confs = read_stream(os.path.join(
-        workdir, "Projects/wf-psf/wf_psf/config/configs.yaml"))
+        workdir, "Projects/wf-psf/config/configs.yaml"))
 
+    # iterate thru generator
     for wf_conf in wf_confs:
-        print('hi', wf_conf)
+        print(wf_conf)
