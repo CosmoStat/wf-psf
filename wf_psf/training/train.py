181a1d306195fbac14977f534959da26f7f871cd
jeipollack <jpollacke@gmail.com>
2023-02-09 15:36:50 +0100

    Rewrote lost property and train methods

--- wf_psf/training/train.py
+++ wf_psf/training/train.py
@@ -15,11 +15,12 @@ from wf_psf.read_config import read_conf
 import os
 import logging
 import wf_psf.io as io
+import wf_psf.psf_models.psf_models as psf_models
 
 logger = logging.getLogger(__name__)
 
 
-class TrainingParams:
+class TrainingParamsHandler:
     def __init__(self, training_params, id_name='-coherent_euclid_200stars'):
         """
         A class to handle training parameters accessed:
@@ -28,17 +29,53 @@ class TrainingParams:
         training input params
         """
 
-        self.training_params = training_params
+        self.training_params = training_params.training
         self.id_name = id_name
-        self.run_id_name = self.training_params.model + self.id_name
+        self.run_id_name = self.model_name + self.id_name
         self.model_save_file = io.get_model_save_file()
         self.saving_optim_hist = {}
 
-    def train(self):
-        # Print GPU and tensorflow info
-        device_name = tf.test.gpu_device_name()
-        logger.info('Found GPU at: {}'.format(device_name))
-        logger.info('tf_version: ' + str(tf.__version__))
+    @property
+    def model_name(self):
+        """PSF Model Name"""
+        return self.training_params.model_params.model_name
+
+    @property
+    def model_params(self):
+        """PSF Model Params"""
+        return self.training_params.model_params
+
+    @property
+    def training_hparams(self):
+        """Training Hyperparameters"""
+        return self.training_params.training_hparams
+
+    @property
+    def training_data_params(self):
+        """Training Data Params"""
+        return self.training_params.data.training
+
+    @property
+    def test_data_params(self):
+        """Test Data Params"""
+        return self.training_params.data.test
+
+    def set_psf_model(self):
+        """Instantiate PSF Model Class"""
+        psf_class = psf_models.PSF_CLASS[self.model_name]
+        return psf_class
+
+
+def train(training_params):
+    # Print GPU and tensorflow info
+    device_name = tf.test.gpu_device_name()
+    logger.info('Found GPU at: {}'.format(device_name))
+    logger.info('tf_version: ' + str(tf.__version__))
+
+    training_handler = TrainingParamsHandler(training_params)
+
+    psf_model = training_handler.set_psf_model()
+    
 
 
 if __name__ == "__main__":
