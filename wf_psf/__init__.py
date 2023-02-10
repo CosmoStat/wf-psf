465da1db9ff7091f57a028f596c686196fa446c5
jeipollack <jpollacke@gmail.com>
2023-02-10 14:18:29 +0100

    Fixed a typo in the package name

--- wf_psf/__init__.py
+++ wf_psf/__init__.py
@@ -12,10 +12,10 @@ from .tf_alt_psf_models import *
 
 from .io import *
 from .utils import *
-from .metrics.metrics import *
+from .metrics import *
 from .train_utils import *
-from .training.train import *
-from .psf_models.psf_models import *
+from .psf_models import *
+from .training import *
 
 from .script_utils import *
 
@@ -23,7 +23,7 @@ from .info import __version__, __about__
 
 __all__ = []  # List of submodules
 __all__ += ['SimPSFToolkit', 'GenPolyFieldPSF']
-__all__ += ['tf_layers', 'tf_modules', 'tf_psf_field', 'tf_mccd_psf_field']
+__all__ += ['tf_layers', 'tf_modules']
 __all__ += ['io', 'graph_utils', 'utils', 'metrics', 'train_utils']
-__all__ += ['tf_alt_psf_models', 'train', 'psf_models']
+__all__ += ['psf_models', 'training']
 __all__ += ['script_utils']
