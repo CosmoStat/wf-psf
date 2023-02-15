0b9ea6d327ed6c4c68834fd17e4cbecaa028feba
jeipollack <jpollacke@gmail.com>
2023-02-15 15:30:38 +0100

    Moved global vars and functions to class FileIOHandler

--- wf_psf/io.py
+++ wf_psf/io.py
@@ -15,104 +15,140 @@ from datetime import datetime
 
 load_dotenv("./.env")
 
-_workdir = os.getenv("WORKDIR")
-_wf_outputs = "wf-outputs"
-_checkpoint = "checkpoint"
-_log_files = "log-files"
-_metrics = "metrics"
-_optim_hist = "optim-hist"
-_plots = "plots"
 
+class FileIOHandler:
+    """FileIOHandler.
 
-def setup_outputs():
-    """Set up Outputs.
-
-    A function to call
-    specific functions
-    to set up output
-    directories and logging.
-
-    """
-    _setup_dirs()
-    _setup_logging()
-
-
-def _setup_dirs():
-    """Setup Directories.
-
-    A function to setup the output
-    directories.
-
-    """
-    _make_output_dir()
-
-    list_of_dirs = (_checkpoint, _log_files, _metrics, _optim_hist, _plots)
-    for dir in list_of_dirs:
-        _make_dir(dir)
-
-
-def _setup_logging():
-    """Setup Logger.
-
-    A function to set up
-    logging.
-
-    """
-    repodir = os.getenv("REPODIR")
-
-    logfile = datetime.now().strftime("train_%Y%m%d%H%M.log")
-
-    logfile = os.path.join(_workdir, _wf_outputs, _log_files, logfile)
-
-    logging.config.fileConfig(
-        os.path.join(repodir, "config/logging.conf"),
-        defaults={"filename": logfile},
-        disable_existing_loggers=False,
-    )
-
-
-def _make_output_dir():
-    """Make Output Directory.
-
-    A function to make the
-    output directory "wf-outputs"
-    inside the directory defined
-    by the $WORKDIR environment
-    variable.
-
-    """
-    pathlib.Path(os.path.join(_workdir, _wf_outputs)).mkdir(exist_ok=True)
-
-
-def _make_dir(dir_name):
-    """Make Directory.
-
-    A function to make a subdirectory
-    inside the parent directory "wf-outputs".
+    A class to manage the output
+    file structure.
 
     Parameters
     ----------
-    dir_name: str
-        Name of directory
+    output_path: str
+        Absolute path to output directory
 
     """
-    pathlib.Path(os.path.join(_workdir, _wf_outputs, dir_name)).mkdir(exist_ok=True)
 
+    def __init__(self, output_path):
+        self._output_path = output_path
+        self._wf_outputs = "wf-outputs"
+        self._checkpoint = "checkpoint"
+        self._log_files = "log-files"
+        self._metrics = "metrics"
+        self._optimizer = "optim-hist"
+        self._plots = "plots"
 
-def get_checkpoint_dir():
-    """Get Checkpoint Directory.
+    def setup_outputs(self):
+        """Set up Outputs.
 
-    A function that returns path
-    of checkpoint directory.
+        A function to call
+        specific functions
+        to set up output
+        directories and logging.
 
-    Returns
-    -------
-    str
-        Absolute path to checkpoint directory
+        Parameters
+        ----------
+        output_path: str
+            Path to output directory
 
-    """
-    return os.path.join(_workdir, _checkpoint)
+        """
+        self._make_output_dir()
+        self._setup_dirs()
+        self._setup_logging()
+
+    def _make_output_dir(self):
+        """Make Output Directory.
+
+        A function to make the
+        output directory "wf-outputs".
+
+        """
+        pathlib.Path(os.path.join(self._output_path, self._wf_outputs)).mkdir(
+            exist_ok=True
+        )
+
+    def _setup_dirs(self):
+        """Setup Directories.
+
+        A function to setup the output
+        directories.
+
+        """
+        list_of_dirs = (
+            self._checkpoint,
+            self._log_files,
+            self._metrics,
+            self._optimizer,
+            self._plots,
+        )
+        for dir in list_of_dirs:
+            self._make_dir(dir)
+
+    def _setup_logging(self):
+        """Setup Logger.
+
+        A function to set up
+        logging.
+
+        """
+        repodir = os.getenv("REPODIR")
+
+        logfile = datetime.now().strftime("train_%Y%m%d%H%M.log")
+
+        logfile = os.path.join(
+            self._output_path, self._wf_outputs, self._log_files, logfile
+        )
+
+        logging.config.fileConfig(
+            os.path.join(repodir, "config/logging.conf"),
+            defaults={"filename": logfile},
+            disable_existing_loggers=False,
+        )
+
+    def _make_dir(self, dir_name):
+        """Make Directory.
+
+        A function to make a subdirectory
+        inside the parent directory "wf-outputs".
+
+        Parameters
+        ----------
+        dir_name: str
+            Name of directory
+
+        """
+        pathlib.Path(os.path.join(self._output_path, self._wf_outputs, dir_name)).mkdir(
+            exist_ok=True
+        )
+
+    def get_checkpoint_dir(self):
+        """Get Checkpoint Directory.
+
+        A function that returns path
+        of checkpoint directory.
+
+        Returns
+        -------
+        str
+            Absolute path to checkpoint directory
+
+        """
+        return os.path.join(self._output_path, self._wf_outputs, self._checkpoint)
+
+    def get_optimizer_dir(self):
+        """Get Optimizer Directory.
+
+        A function that returns path
+        of optimizer directory.
+
+        Returns
+        -------
+        str
+            Absolute path to optimizer directory
+
+        """
+        return os.path.join(self._output_path, self._wf_outputs, self._optimizer)
 
 
 if __name__ == "__main__":
-    setup_dirs()
+    _setup_dirs()
