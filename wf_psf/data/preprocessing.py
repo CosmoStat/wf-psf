00a06538673c80768c9de15a51bcfe3f49a1c8ac
jeipollack <jpollacke@gmail.com>
2023-03-01 13:01:00 +0100

    Added training and test Data Handling Classes

--- wf_psf/data/preprocessing.py
+++ wf_psf/data/preprocessing.py
@@ -63,31 +63,11 @@ class TrainingDataHandler:
         self.n_bins_lambda = n_bins_lambda
        # self.sed_data = self._convert_SED_format_to_tensor_flow()
         self.sed_data = [
-            utils.generate_SED_elems_in_tensorflow(_sed, self.simPSF, n_bins=self.n_bins_lambda)
+            utils.generate_SED_elems_in_tensorflow(_sed, self.simPSF, n_bins=self.n_bins_lambda, tf_dtype=tf.float32)
             for _sed in self.train_dataset["SEDs"]
         ]
         self.sed_data = tf.transpose(self.sed_data, perm=[0, 2, 1])
-        breakpoint()
-
-    @property
-    def training_data_filename(self):
-        return self.training_data_params.file
-
-    def _convert_SED_format_to_tensor_flow(self):
-        """Temporary Function"""
-        # Initialize the SED data list
-        packed_SED_data = [
-            utils.alternative_generate_SED_elems(_sed, self.simPSF, n_bins=self.n_bins_lambda)
-            for _sed in self.train_dataset["SEDs"]
-        ]
-
-        # Prepare the inputs for the training
-       # tf_packed_SED_data = tf.convert_to_tensor(packed_SED_data, dtype=tf.float32)
-
-        tf_packed_SED_data = tf.transpose(packed_SED_data, perm=[0, 2, 1])
-        return tf_packed_SED_data
-
-
+      
 
 class TestDataHandler:
     """Test Data.
@@ -95,26 +75,20 @@ class TestDataHandler:
     A class to handle test data.
     
     """
-    def __init__(self, test_data_params):
+    def __init__(self, test_data_params, simPSF, n_bins_lambda):
         self.test_data_params = test_data_params
         self.test_dataset = load_dataset_dict(self.test_data_params.file)
         self.test_dataset["stars"] = tf.convert_to_tensor(self.test_dataset["stars"], dtype=tf.float32)
         self.test_dataset["positions"] = tf.convert_to_tensor(self.test_dataset["positions"], dtype=tf.float32)
+        self.simPSF = simPSF
+        self.n_bins_lambda = n_bins_lambda
         self.sed_data = [
             utils.generate_SED_elems_in_tensorflow(_sed, self.simPSF, n_bins=self.n_bins_lambda)
             for _sed in self.test_dataset["SEDs"]
         ]
-    # Prepare validation data inputs
-    # JP: not necessary -- just making a duplicate copy in memory
-   # validation_SEDs = test_SEDs
-   # tf_validation_pos = tf_test_pos
-   # tf_validation_stars = tf_test_stars
-
-
-
-        # Prepare the inputs for the validation
-        tf_validation_packed_SED_data = tf.convert_to_tensor(validation_packed_SED_data, dtype=tf.float32)
-        tf_validation_packed_SED_data = tf.transpose(tf_validation_packed_SED_data, perm=[0, 2, 1])
+        self.sed_data = tf.transpose(self.sed_data, perm=[0, 2, 1])
+      
+   
 
 
 
