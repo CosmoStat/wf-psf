c4e5b154eae0b21d80243432aaf40740b1398445
jeipollack <jpollacke@gmail.com>
2023-03-27 13:30:58 +0200

    Testing refactoring d.r.y.  multicycles training

--- wf_psf/training/train.py
+++ wf_psf/training/train.py
@@ -146,6 +146,33 @@ class TrainingParamsHandler:
         """
         return self.training_params.data.test
 
+    @property
+    def _filepath_chkp_callback(self):
+         return (self.checkpoint_dir
+            + "/"
+            + "chkp_callback_"
+            + self.model_name
+            + self.id_name
+            + "_cycle1"
+        )
+        
+        
+    def _prepare_callbacks(self):
+        # Prepare to save the model as a callback
+        # -----------------------------------------------------
+        logger.info(f"Preparing Keras model callback...")
+        return tf.keras.callbacks.ModelCheckpoint(
+            self._filepath_chkp_callback,
+            monitor="mean_squared_error",
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=True,
+            mode="min",
+            save_freq="epoch",
+            options=None,
+        )
+
+
 
 def get_gpu_info():
     """Get GPU Information.
@@ -161,6 +188,7 @@ def get_gpu_info():
     """
     device_name = tf.test.gpu_device_name()
     return device_name
+
 
 
 def train(training_params, output_dirs):
@@ -190,43 +218,25 @@ def train(training_params, output_dirs):
     logger.info(f"PSF Model class: `{training_handler.model_name}` initialized...")
     # Model Training
     # Prepare the saving callback
-    # Prepare to save the model as a callback
-    # -----------------------------------------------------
-    logger.info(f"Preparing Keras model callback...")
-    filepath_chkp_callback = (
-        training_handler.checkpoint_dir
-        + "/"
-        + "chkp_callback_"
-        + training_handler.model_name
-        + training_handler.id_name
-        + "_cycle1"
-    )
-    model_chkp_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath_chkp_callback,
-        monitor="mean_squared_error",
-        verbose=1,
-        save_best_only=True,
-        save_weights_only=True,
-        mode="min",
-        save_freq="epoch",
-        options=None,
-    )
+    #model_chkp_callback = training_handler._prepare_callbacks()    
+
+
     # -----------------------------------------------------
     # Instantiate Simulated PSF Toolkit
     logger.info(f"Instantiating simPSF toolkit...")
     simPSF = psf_models.simPSF(training_handler.model_params)
 
-    # Prepare the optimisers
-    param_optim = tfa.optimizers.RectifiedAdam(
-        learning_rate=training_handler.training_multi_cycle_params.learning_rate_param_multi_cycle[
-            0
-        ]
-    )
-    non_param_optim = tfa.optimizers.RectifiedAdam(
-        learning_rate=training_handler.training_multi_cycle_params.learning_rate_non_param_multi_cycle[
-            0
-        ]
-    )
+    # # Prepare the optimisers
+    # param_optim = tfa.optimizers.RectifiedAdam(
+    #     learning_rate=training_handler.training_multi_cycle_params.learning_rate_param_multi_cycle[
+    #         0
+    #     ]
+    # )
+    # non_param_optim = tfa.optimizers.RectifiedAdam(
+    #     learning_rate=training_handler.training_multi_cycle_params.learning_rate_non_param_multi_cycle[
+    #         0
+    #     ]
+    # )
     # -----------------------------------------------------
     # Get training data
     logger.info(f"Fetching and preprocessing training and test data...")
@@ -241,55 +251,55 @@ def train(training_params, output_dirs):
         training_handler.model_params.n_bins_lda,
     )
 
-    print("Starting training cycle 1..")
-    start_cycle1 = time.time()
+    # print("Starting training cycle 1..")
+    # start_cycle1 = time.time()
 
-    tf_semiparam_field, hist_param, hist_non_param = train_utils.general_train_cycle(
-        # poly model
-        psf_model,
-        # training data
-        inputs=[training_data.train_dataset["positions"], training_data.sed_data],
-        outputs=training_data.train_dataset["noisy_stars"],
-        validation_data=(
-            [test_data.test_dataset["positions"], test_data.sed_data],
-            test_data.test_dataset["stars"],
-        ),
-        batch_size=training_handler.training_hparams.batch_size,
-        learning_rate_param=training_handler.training_multi_cycle_params.learning_rate_param_multi_cycle[
-            0
-        ],
-        learning_rate_non_param=training_handler.training_multi_cycle_params.learning_rate_non_param_multi_cycle[
-            0
-        ],
-        n_epochs_param=training_handler.training_hparams.n_epochs_param[0],
-        n_epochs_non_param=training_handler.training_hparams.n_epochs_non_param[0],
-        param_optim=param_optim,
-        non_param_optim=non_param_optim,
-        param_loss=None,
-        non_param_loss=None,
-        param_metrics=None,
-        non_param_metrics=None,
-        param_callback=None,
-        non_param_callback=None,
-        general_callback=[model_chkp_callback],
-        first_run=True,
-        cycle_def=training_handler.training_multi_cycle_params.cycle_def,
-        use_sample_weights=training_handler.model_params.use_sample_weights,
-        verbose=2,
-    )
+    # tf_semiparam_field, hist_param, hist_non_param = train_utils.general_train_cycle(
+    #     # poly model
+    #     psf_model,
+    #     # training data
+    #     inputs=[training_data.train_dataset["positions"], training_data.sed_data],
+    #     outputs=training_data.train_dataset["noisy_stars"],
+    #     validation_data=(
+    #         [test_data.test_dataset["positions"], test_data.sed_data],
+    #         test_data.test_dataset["stars"],
+    #     ),
+    #     batch_size=training_handler.training_hparams.batch_size,
+    #     learning_rate_param=training_handler.training_multi_cycle_params.learning_rate_param_multi_cycle[
+    #         0
+    #     ],
+    #     learning_rate_non_param=training_handler.training_multi_cycle_params.learning_rate_non_param_multi_cycle[
+    #         0
+    #     ],
+    #     n_epochs_param=training_handler.training_hparams.n_epochs_param[0],
+    #     n_epochs_non_param=training_handler.training_hparams.n_epochs_non_param[0],
+    #     param_optim=param_optim,
+    #     non_param_optim=non_param_optim,
+    #     param_loss=None,
+    #     non_param_loss=None,
+    #     param_metrics=None,
+    #     non_param_metrics=None,
+    #     param_callback=None,
+    #     non_param_callback=None,
+    #     general_callback=[model_chkp_callback],
+    #     first_run=True,
+    #     cycle_def=training_handler.training_multi_cycle_params.cycle_def,
+    #     use_sample_weights=training_handler.model_params.use_sample_weights,
+    #     verbose=2,
+    # )
 
-    end_cycle1 = time.time()
-    print("Cycle1 elapsed time: %f" % (end_cycle1 - start_cycle1))
+    # end_cycle1 = time.time()
+    # print("Cycle1 elapsed time: %f" % (end_cycle1 - start_cycle1))
 
-    # Save optimisation history in the saving dict
-    saving_optim_hist = {}
-    if hist_param is not None:
-        saving_optim_hist["param_cycle1"] = hist_param.history
-    if psf_model.ids != "param" and hist_non_param is not None:
-        saving_optim_hist["nonparam_cycle1"] = hist_non_param.history
+    # # Save optimisation history in the saving dict
+    # saving_optim_hist = {}
+    # if hist_param is not None:
+    #     saving_optim_hist["param_cycle1"] = hist_param.history
+    # if psf_model.ids != "param" and hist_non_param is not None:
+    #     saving_optim_hist["nonparam_cycle1"] = hist_non_param.history
 
     # Perform all the necessary cycles
-    current_cycle = 1
+    current_cycle = 0
 
     while (
         training_handler.training_hparams.multi_cycle_params.total_cycles
@@ -318,16 +328,7 @@ def train(training_params, output_dirs):
             + "_cycle"
             + str(current_cycle)
         )
-        model_chkp_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath_chkp_callback,
-            monitor="mean_squared_error",
-            verbose=1,
-            save_best_only=True,
-            save_weights_only=True,
-            mode="min",
-            save_freq="epoch",
-            options=None,
-        )
+        model_chkp_callback = training_handler._prepare_callbacks()  
 
         # Prepare the optimisers
         param_optim = tfa.optimizers.RectifiedAdam(
