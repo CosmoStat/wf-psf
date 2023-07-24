4ee19e507a559945f4a669905ac9992388573bc6
jeipollack <jpollacke@gmail.com>
2023-07-24 21:41:29 +0200

    Updated metrics test for validation

--- wf_psf/tests/metrics_test.py
+++ wf_psf/tests/metrics_test.py
@@ -20,7 +20,7 @@ metrics_params = RecursiveNamespace(
     use_callback=False,
     saved_training_cycle="cycle2",
     chkp_save_path="checkpoint",
-    id_name="-coherent_euclid_200stars",
+    id_name="_sample_w_bis1_2k",
     model_params=RecursiveNamespace(
         model_name="poly",
         use_callback=False,
@@ -28,7 +28,7 @@ metrics_params = RecursiveNamespace(
         sed_extrapolate=True,
         sed_interp_kind="linear",
         sed_sigma=0,
-        n_bins_lda=8,
+        n_bins_lda=20,
         output_Q=3,
         oversampling_rate=3,
         output_dim=32,
@@ -38,7 +38,7 @@ metrics_params = RecursiveNamespace(
         x_lims=[0.0, 1000.0],
         y_lims=[0.0, 1000.0],
         param_hparams=RecursiveNamespace(
-            l2_param=0.0, n_zernikes=15, d_max=2, save_optim_history_param=True
+            l2_param=0.0, n_zernikes=45, d_max=2, save_optim_history_param=True
         ),
         nonparam_hparams=RecursiveNamespace(
             d_max_nonparam=5,
@@ -90,9 +90,9 @@ metrics_params = RecursiveNamespace(
     ),
 )
 
-chkp_dir = "../wf-outputs/checkpoint"
-optim_dir = "../wf-outputs/optim-hist"
-metrics_output = "../wf-outputs/metrics"
+chkp_dir = "wf_psf/tests/data/validation/checkpoint_paper"
+# optim_dir = "../wf-outputs/optim-hist"
+metrics_output = "wf_psf/tests/data/wf-outputs/metrics"
 
 
 @pytest.fixture(scope="module", params=[metrics_params])
@@ -105,9 +105,45 @@ def test_metrics_params(metrics: RecursiveNamespace):
     print(metric_params)
 
 
+def test_eval_metrics_polychromatic_lowres(
+    training_params, training_data, test_dataset, psf_model
+):
+    metrics_handler = MetricsParamsHandler(metrics_params, training_params)
+    cycle = 2
+
+    # Load paper results
+    filename = "/Users/jenniferpollack/Projects/wf-psf/wf_psf/tests/data/validation/metrics_paper/wavediff-original/metrics-poly_sample_w_bis1_2k.npy"
+
+    truth_poly_metric = {
+        "rmse": 6.379096e-05,
+        "rel_rmse": 0.8615310303866863,
+        "std_rmse": 9.822091e-06,
+        "std_rel_rmse": 0.21410740446299314,
+    }
+
+    ## Prepare models
+    # Prepare np input
+    simPSF_np = training_data.simPSF
+
+    # Load the model's weights
+    psf_model.load_weights(
+        train.filepath_chkp_callback(
+            chkp_dir,
+            training_params.model_params.model_name,
+            training_params.id_name,
+            cycle,
+        )
+    )
+
+    poly_metric = metrics_handler.evaluate_metrics_polychromatic_lowres(
+        psf_model, simPSF_np, test_dataset
+    )
+    breakpoint()
+
+
 def test_evaluate_metrics_opd(training_params, training_data, test_dataset, psf_model):
     metrics_handler = MetricsParamsHandler(metrics_params, training_params)
-    cycle = 1
+    cycle = 2
 
     ## Prepare models
     # Prepare np input
@@ -132,7 +168,7 @@ def test_eval_metrics_mono_rmse(
     training_params, training_data, test_dataset, psf_model
 ):
     metrics_handler = MetricsParamsHandler(metrics_params, training_params)
-    cycle = 1
+    cycle = 2
 
     ## Prepare models
     # Prepare np input
@@ -149,31 +185,6 @@ def test_eval_metrics_mono_rmse(
     )
 
     mono_metric = metrics_handler.evaluate_metrics_mono_rmse(
-        psf_model, simPSF_np, test_dataset
-    )
-
-
-def test_eval_metrics_polychromatic_lowres(
-    training_params, training_data, test_dataset, psf_model
-):
-    metrics_handler = MetricsParamsHandler(metrics_params, training_params)
-    cycle = 1
-
-    ## Prepare models
-    # Prepare np input
-    simPSF_np = training_data.simPSF
-
-    # Load the model's weights
-    psf_model.load_weights(
-        train.filepath_chkp_callback(
-            chkp_dir,
-            training_params.model_params.model_name,
-            training_params.id_name,
-            cycle,
-        )
-    )
-
-    poly_metric = metrics_handler.evaluate_metrics_polychromatic_lowres(
         psf_model, simPSF_np, test_dataset
     )
 
@@ -206,7 +217,7 @@ def test_evaluate_metrics_shape(
 def test_evaluate_model(
     training_params: RecursiveNamespace, training_data, test_data, psf_model
 ):
-    cycle = 1
+    cycle = 2
     evaluate_model(
         metrics_params,
         training_params,
