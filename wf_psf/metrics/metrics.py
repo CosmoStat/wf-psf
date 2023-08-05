adc93750cee3eb83c67b03adfab5a87d09193db3
jwang11 <jingwei.wang@cea.fr>
2023-08-05 16:58:59 +0200

    delete print, less lambdas

--- wf_psf/metrics/metrics.py
+++ wf_psf/metrics/metrics.py
@@ -1483,7 +1483,6 @@ def compute_mono_psf(
                     GT_pred_e1_HSM.append(GT_pred_moments[ii].observed_shape.g1)
                     GT_pred_e2_HSM.append(GT_pred_moments[ii].observed_shape.g2)
                     GT_pred_R2_HSM.append(2 * (GT_pred_moments[ii].moments_sigma ** 2))
-                    print(ii + ep_low_lim)
                     ell_loc.append(ii + ep_low_lim)
                     flag[ii + ep_low_lim] = 1
 
