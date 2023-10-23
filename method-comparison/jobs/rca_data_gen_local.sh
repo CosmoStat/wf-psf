8125ea2adba4c27f9b1edf4db295ece92a32721a
jeipollack <jpollacke@gmail.com>
2023-10-23 13:16:01 +0200

    Removed unused dir

--- method-comparison/jobs/rca_data_gen_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/method-comparison/scripts/dataset-conversion-RCA-intrapixshifts.py \
-    --base_repo_path /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/ \
-    --rca_saving_path /Users/tliaudat/Documents/PhD/codes/WF_PSF/github/wf-psf/method-comparison/compatible-datasets/rca_shifts/ \
