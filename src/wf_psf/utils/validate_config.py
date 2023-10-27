c17729619898bb80b09f194e61887d09d4a498c1
jeipollack <jpollacke@gmail.com>
2023-10-27 21:49:21 +0200

    Updated docstrings

--- src/wf_psf/utils/validate_config.py
+++ src/wf_psf/utils/validate_config.py
@@ -22,13 +22,13 @@ def check_if_positive_int(*conf_param):
 
     Parameters
     ----------
-        *conf_param: list
-            A list containing configuration parameters
+    *conf_param: list
+        A list containing configuration parameters
 
     Returns
     -------
-        bool
-            Returns a boolean
+    bool
+        Returns a boolean
     """
 
     return bool(isinstance(conf_param[0], int) > 0)
@@ -42,13 +42,13 @@ def check_if_positive_float(*conf_param):
 
     Parameters
     ----------
-        *conf_param: list
-            A list containing configuration parameters
+    *conf_param: list
+        A list containing configuration parameters
 
     Returns
     -------
-        bool
-            Returns a boolean
+    bool
+        Returns a boolean
     """
     return bool(isinstance(conf_param[0], float) > 0)
 
@@ -61,15 +61,15 @@ def get_attr_from_RN(rn, key):
 
     Parameters
     ----------
-        rn: RecursiveNamespace instance
-            A RecursiveNamespace instance
-        key: str
-            A string representation of an object in the namespace of the RecursiveNamespace.
-    
+    rn: RecursiveNamespace instance
+        A RecursiveNamespace instance
+    key: str
+        A string representation of an object in the namespace of the RecursiveNamespace.
+
     Returns
     -------
-        value
-            The value of the attribute of an object.
+    value
+        The value of the attribute of an object.
 
     """
     return rn.__getattribute__(key)
@@ -83,16 +83,16 @@ class ValidateConfig:
 
     Parameters
     ----------
-        file_handler: object
-            An instance of the FileIOHandler
-        config_file: RecursiveNamespace object
-            RecursiveNamespace object containing the training configuration parameters
-        config_type: str
-            A string representing the type of configuration file, i.e. data, training, etc.
-        
+    file_handler: object
+        An instance of the FileIOHandler
+    config_file: RecursiveNamespace object
+        RecursiveNamespace object containing the training configuration parameters
+    config_type: str
+        A string representing the type of configuration file, i.e. data, training, etc.
+
     """
 
-    def __init__(self,  file_handler, config_file, config_type):
+    def __init__(self, file_handler, config_file, config_type):
         self.file_handler = file_handler
         self.config_file = config_file
         self.validate_dict = validate_dict[config_type]
@@ -116,6 +116,17 @@ class ValidateConfig:
 
         A method to check if the config file exists.
 
+        Parameters
+        ----------
+        conf_param: list
+            A list of configuration of parameters
+
+        Returns
+        -------
+        bool
+            Returns a boolean
+
+
         """
         return bool(
             os.path.exists(os.path.join(self.file_handler.config_path, conf_param[0]))
@@ -125,14 +136,35 @@ class ValidateConfig:
         """Check if None.
 
         A method to check config file is none.
+
+        Parameters
+        ----------
+        config_param: list
+            A list of configuration parameters
+
+        Returns
+        -------
+        bool
+            Returns a boolean
         """
         return bool(conf_param[0] is None)
 
     def check_if_equals_name(self, *conf_params):
-        """Check Config Param Equals Name.
+        """Check if Config Param Equals Name.
 
         A method to check config param equals the
         expected name.
+
+        Parameters
+        ----------
+        config_params: list
+            A list of configuration parameters.
+
+        Returns
+        -------
+        bool
+            Returns a boolean
+
         """
         return bool(conf_params[0] in conf_params[1])
 
@@ -144,7 +176,14 @@ class ValidateConfig:
 
         Parameters
         ----------
-            config_param
+        config_param: list
+            A list of configuration parameters
+
+        Returns
+        -------
+        bool
+            Returns a boolean
+
         """
         return bool(type(conf_param[0]) is bool)
 
@@ -154,7 +193,6 @@ class ValidateConfig:
         A function to validate the config parameter
         according to the condition.
         """
-        print(config_param, conditions)
 
         condition_type = {
             "check_file_exists": self.check_config_exists,
@@ -168,20 +206,23 @@ class ValidateConfig:
         logger.info("Evaluating conditions...")
         try:
             for condition in conditions["function"]:
-                print(condition)
                 condition_type[condition]
 
                 if condition_type[condition](config_param, conditions["name"]) is True:
-                    print("{} fulfills condition {}".format(config_param, condition))
-                    print(" ")
+                    logger.info(
+                        "{} fulfills condition {}".format(config_param, condition)
+                    )
+                    logger.info(" ")
                     break
                 else:
-                    print(
+                    logger.info(
                         "{} does not fulfill condition {}.".format(
                             config_param, condition
                         )
                     )
-                    print(" ")
+                    logger.info(" ")
                     continue
         except KeyError:
-            logger.exception("Config Param does not fulfill condition.")
+            logger.exception(
+                "Config Param {} does not fulfill conditions.".format(config_param)
+            )
