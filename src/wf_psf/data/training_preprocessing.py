3a6c718f5db62a0019067c2c1b84865897ae0ac0
jeipollack <jpollacke@gmail.com>
2024-02-07 23:37:54 +0100

    Reduced to single DataHandler class

--- src/wf_psf/data/training_preprocessing.py
+++ src/wf_psf/data/training_preprocessing.py
@@ -13,7 +13,7 @@ import tensorflow_addons as tfa
 import os
 
 
-class TrainingDataHandler:
+class DataHandler:
     """Training Data Handler.
 
     A class to manage training data.
@@ -29,70 +29,34 @@ class TrainingDataHandler:
 
     """
 
-    def __init__(self, training_data_params, simPSF, n_bins_lambda):
-        self.training_data_params = training_data_params
-        self.train_dataset = np.load(
+    def __init__(self, data_type, data_params, simPSF, n_bins_lambda):
+        self.data_params = data_params.__dict__[data_type]
+        self.dataset = np.load(
             os.path.join(
-                self.training_data_params.data_dir, self.training_data_params.file
+                self.data_params.data_dir, self.data_params.file
             ),
             allow_pickle=True,
         )[()]
-        self.train_dataset["positions"] = tf.convert_to_tensor(
-            self.train_dataset["positions"], dtype=tf.float32
+        self.dataset["positions"] = tf.convert_to_tensor(
+            self.dataset["positions"], dtype=tf.float32
         )
-        self.train_dataset["noisy_stars"] = tf.convert_to_tensor(
-            self.train_dataset["noisy_stars"], dtype=tf.float32
-        )
+        
+        if "train" in self.data_params.file:
+            self.dataset["noisy_stars"] = tf.convert_to_tensor(
+                self.dataset["noisy_stars"], dtype=tf.float32
+            )
+        elif "test" in self.data_params.file:
+            self.dataset["stars"] = tf.convert_to_tensor(
+            self.dataset["stars"], dtype=tf.float32
+            )
+            
         self.simPSF = simPSF
         self.n_bins_lambda = n_bins_lambda
         self.sed_data = [
             utils.generate_SED_elems_in_tensorflow(
                 _sed, self.simPSF, n_bins=self.n_bins_lambda, tf_dtype=tf.float64
             )
-            for _sed in self.train_dataset["SEDs"]
-        ]
-        self.sed_data = tf.convert_to_tensor(self.sed_data, dtype=tf.float32)
-        self.sed_data = tf.transpose(self.sed_data, perm=[0, 2, 1])
-
-
-class TestDataHandler:
-    """Test Data Handler.
-
-    A class to handle test data for model validation.
-
-    Parameters
-    ----------
-    test_data_params: Recursive Namespace object
-        Recursive Namespace object containing test data parameters
-    simPSF: object
-        PSFSimulator instance
-    n_bins_lambda: int
-        Number of bins in wavelength
-
-    """
-
-    def __init__(self, test_data_params, simPSF, n_bins_lambda):
-        self.test_data_params = test_data_params
-        self.test_dataset = np.load(
-            os.path.join(self.test_data_params.data_dir, self.test_data_params.file),
-            allow_pickle=True,
-        )[()]
-        self.test_dataset["stars"] = tf.convert_to_tensor(
-            self.test_dataset["stars"], dtype=tf.float32
-        )
-        self.test_dataset["positions"] = tf.convert_to_tensor(
-            self.test_dataset["positions"], dtype=tf.float32
-        )
-
-        # Prepare validation data inputs
-        self.simPSF = simPSF
-        self.n_bins_lambda = n_bins_lambda
-
-        self.sed_data = [
-            utils.generate_SED_elems_in_tensorflow(
-                _sed, self.simPSF, n_bins=self.n_bins_lambda, tf_dtype=tf.float64
-            )
-            for _sed in self.test_dataset["SEDs"]
+            for _sed in self.dataset["SEDs"]
         ]
         self.sed_data = tf.convert_to_tensor(self.sed_data, dtype=tf.float32)
         self.sed_data = tf.transpose(self.sed_data, perm=[0, 2, 1])
