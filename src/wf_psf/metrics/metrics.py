c9a4b957f012c0118a056beb98a08f8993fb5cfa
jeipollack <jpollacke@gmail.com>
2024-03-13 19:02:03 +0100

    Updated import build_psf_models from psf_models.psf_models

--- src/wf_psf/metrics/metrics.py
+++ src/wf_psf/metrics/metrics.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import galsim as gs
 import wf_psf.utils.utils as utils
-from wf_psf.psf_models.tf_psf_field import build_PSF_model
+from wf_psf.psf_models.psf_models import build_PSF_model
 from wf_psf.psf_models import tf_psf_field as psf_field
 from wf_psf.sims import psf_simulator as psf_simulator
 import logging
