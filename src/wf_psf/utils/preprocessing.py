d45d299d1cf150df038b297d7a2ea7c0b0297ae6
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-04-04 10:00:49 +0200

    add preprocessing utils

---
+++ src/wf_psf/utils/preprocessing.py
@@ -0,0 +1,60 @@
+"""Preprocessing.
+
+A module with utils to preprocess data.
+
+:Author: Tobias Liaudat <tobias.liaudat@cea.fr>
+
+"""
+
+import numpy as np
+
+
+def shift_x_y_to_zk1_2_wavediff(dxy, tel_focal_length=24.5, tel_diameter=1.2):
+    """Compute Zernike 1(2) for a given shifts in x(y) in WaveDifff conventions.
+
+    All inputs should be in [m].
+    A displacement of, for example, 0.5 pixels should be scaled with the corresponding pixel scale,
+    e.g. 12[um], to get a displacement in [m], which would be `dxy=0.5*12e-6`.
+
+    """
+    reference_pix_sampling = 12e-6
+    zernike_norm_factor = 2.
+
+    # return zernike_norm_factor * (dx/reference_pix_sampling) / (tel_focal_length * tel_diameter / 2)
+    return zernike_norm_factor * (tel_diameter/2) * np.sin(np.arctan((dxy/reference_pix_sampling)/tel_focal_length)) * 3.
+
+
+def defocus_to_zk4_zemax(dz, tel_focal_length=24.5, tel_diameter=1.2):
+    """Compute Zernike 4 value for a given defocus in zemax conventions.
+
+    All inputs should be in [m].
+    """
+    # Base calculation 
+    zk4 = dz / (8. * (tel_focal_length/tel_diameter)**2)
+    # Apply Z4 normalisation 
+    # This step depends on the normalisation of the Zernike basis used 
+    zk4 /= np.sqrt(3)
+    # Convert to waves with a reference of 800nm
+    zk4 /= 800e-9
+    # Remove the peak to valley value
+    zk4 /= 2.
+
+    return  zk4
+
+
+def defocus_to_zk4_wavediff(dz, tel_focal_length=24.5, tel_diameter=1.2):
+    """Compute Zernike 4 value for a given defocus in WaveDifff conventions.
+
+    All inputs should be in [m].
+    """
+    # Base calculation 
+    zk4 = dz / (8. * (tel_focal_length/tel_diameter)**2)
+    # Remove the peak to valley value
+    zk4 /= 2.
+
+    return  zk4
+
+
+
+
+
