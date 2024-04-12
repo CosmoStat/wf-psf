6da64602ff90a17506831915336d62abf5622f31
jeipollack <jpollacke@gmail.com>
2024-04-12 10:37:21 +0200

    Refactor: Add TFGroundTruthPhysicalField and TFGroundTruthSemiParametricField to psf model registry

--- src/wf_psf/psf_models/tf_psf_field.py
+++ src/wf_psf/psf_models/tf_psf_field.py
@@ -7,62 +7,184 @@ from wf_psf.psf_models.tf_layers import (
     TFBatchMonochromaticPSF,
     TFPhysicalLayer,
 )
+from wf_psf.psf_models.psf_model_semiparametric import TFSemiParametricField
+from wf_psf.data.training_preprocessing import get_obs_positions, get_zernike_prior
+from wf_psf.psf_models import psf_models as psfm
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@psfm.register_psfclass
+class GroundTruthSemiParamFieldFactory(psfm.PSFModelBaseFactory):
+    """Factory class for the Tensor Flow Ground Truth Physical PSF Field Model.
+
+    This factory class is responsible for instantiating instances
+    of the TensorFlow Ground Truth SemiParametric PSF Field Model.
+    It is registered with the PSF model factory registry.
+
+    Parameters
+    ----------
+    ids : tuple
+        A tuple containing identifiers for the factory class.
+
+    Methods
+    -------
+    get_model_instance(model_params, training_params, data=None, coeff_mat=None)
+        Instantiates an instance of the TensorFlow SemiParametric `poly` Field class with the provided parameters.
+    """
+
+    ids = ("ground_truth_poly",)
+
+    def get_model_instance(
+        self, model_params, training_params, data, dataset, coeff_mat=None
+    ):
+        return TFGroundTruthSemiParametricField(
+            model_params, training_params, dataset, coeff_mat
+        )
+
+
+@psfm.register_psfclass
+class GroundTruthPhysicalFieldFactory(psfm.PSFModelBaseFactory):
+    """Factory class for the Tensor Flow Ground Truth Physical PSF Field Model.
+
+    This factory class is responsible for instantiating instances of the TensorFlow Ground Truth Physical PSF Field Model.
+    It is registered with the PSF model factory registry.
+
+    Parameters
+    ----------
+    ids : tuple
+        A tuple containing identifiers for the factory class.
+
+    Methods
+    -------
+    get_model_instance(model_params, training_params, data=None, coeff_mat=None)
+        Instantiates an instance of the TensorFlow Physical Polychromatic Field class with the provided parameters.
+    """
+
+    ids = ("ground_truth_physical_poly",)
+
+    def get_model_instance(
+        self, model_params, training_params, data, dataset, coeff_mat=None
+    ):
+        return TFGroundTruthPhysicalField(
+            model_params, training_params, data, coeff_mat
+        )
+
+
+def get_ground_truth_zernike(data):
+    """Get Ground Truth Zernikes from the provided dataset.
+
+    This method concatenates the Ground Truth Zernike from both the training
+    and test datasets.
+
+    Parameters
+    ----------
+    data : DataConfigHandler
+        Object containing training and test datasets.
+
+    Returns
+    -------
+    tf.Tensor
+        Tensor containing the observed positions of the stars.
+
+    Notes
+    -----
+    The Zernike Ground Truth are obtained by concatenating the
+    Ground Truth Zernikes from both the training and test datasets
+    along the 0th axis.
+
+    """
+    zernike_ground_truth = np.concatenate(
+        (
+            data.training_data.dataset["zernike_GT"],
+            data.test_data.dataset["zernike_GT"],
+        ),
+        axis=0,
+    )
+    return tf.convert_to_tensor(zernike_ground_truth, dtype=tf.float32)
+
+
+class TFGroundTruthSemiParametricField(TFSemiParametricField):
+    """A TensorFlow-based Ground Truth Semi-Parametric PSF Field Model.
+
+    This class represents a ground truth semi-parametric PSF (Point Spread Function)
+    field model implemented using TensorFlow. The model is designed to generate
+    ground truth PSF fields based on provided parameters and coefficient matrices.
+
+    Parameters
+    ----------
+    model_params : RecursiveNamespace
+        A RecursiveNamespace object containing parameters for configuring the PSF model.
+    training_params : RecursiveNamespace
+        A RecursiveNamespace object containing training hyperparameters for the PSF model.
+    dataset : dict or None
+        A dictionary containing dataset [test, training] required for model initialization, including the
+        coefficient matrix ('C_poly') for the semi-parametric PSF field.
+    coeff_mat : Tensor or None, optional
+        The coefficient matrix defining the semi-parametric PSF field model. This matrix
+        specifies the coefficients for the Zernike polynomials used in the PSF field.
+        If None, the model will be initialized without a coefficient matrix.
+
+    Attributes
+    ----------
+    GT_tf_semiparam_field : TFSemiParametricField
+        An instance of the TensorFlow-based Semi-Parametric PSF Field Model used for
+        generating ground truth PSF fields.
+
+    Methods
+    -------
+    __init__(model_params, training_params, data, coeff_mat)
+        Initializes the TFGroundTruthSemiParametricField instance.
+    """
+
+    def __init__(self, model_params, training_params, dataset, coeff_mat=None):
+        super().__init__(model_params, training_params, coeff_mat)
+
+        # For the Ground truth model
+        self.tf_poly_Z_field.assign_coeff_matrix(dataset["C_poly"])
+        self.set_zero_nonparam()
 
 
 class TFGroundTruthPhysicalField(tf.keras.Model):
-    """Ground Truth PSF field forward model with a physical layer
+    """Ground Truth PSF field forward model with a physical layer.
 
     Ground truth PSF field used for evaluation purposes.
 
     Parameters
     ----------
-    zernike_maps: Tensor(n_batch, opd_dim, opd_dim)
-        Zernike polynomial maps.
-    obscurations: Tensor(opd_dim, opd_dim)
-        Predefined obscurations of the phase.
-    batch_size: int
-        Batch size
-    obs_pos: Tensor(n_stars, 2)
-        The positions of all the stars
-    zks_prior: Tensor(n_stars, n_zks)
-        The Zernike coeffients of the prior for all the stars
-    output_Q: float
-        Oversampling used. This should match the oversampling Q used to generate
-        the diffraction zero padding that is found in the input `packed_SEDs`.
-        We call this other Q the `input_Q`.
-        In that case, we replicate the original sampling of the model used to
-        calculate the input `packed_SEDs`.
-        The final oversampling of the generated PSFs with respect to the
-        original instrument sampling depend on the division `input_Q/output_Q`.
-        It is not recommended to use `output_Q < 1`.
-        Although it works with float values it is better to use integer values.
-    output_dim: int
-        Output dimension of the PSF stamps.
-
+    ids : tuple
+        A tuple storing the string attribute of the PSF model class
+    model_params : Recursive Namespace
+        A Recursive Namespace object containing parameters for this PSF model class
+    training_params : Recursive Namespace
+        A Recursive Namespace object containing training hyperparameters for this PSF model class
+    data : DataConfigHandler object
+        A DataConfigHandler object containing training and tests datasets
+    coeff_mat : Tensor or None
+        Initialization of the coefficient matrix defining the parametric psf field model
 
     """
 
-    def __init__(
-        self,
-        zernike_maps,
-        obscurations,
-        batch_size,
-        obs_pos,
-        zks_prior,
-        output_Q,
-        output_dim=64,
-        name="TF_GT_physical_field",
-    ):
+    def __init__(self, model_params, training_params, data, coeff_mat):
         super(TFGroundTruthPhysicalField, self).__init__()
 
+        logger.info("Initialising TFGroundTruthPhysicalField class...")
         # Inputs: oversampling used
-        self.output_Q = output_Q
-        self.n_zks_total = tf.shape(zernike_maps)[0].numpy()
+        self.output_Q = model_params.output_Q
 
         # Inputs: TF_physical_layer
-        self.obs_pos = obs_pos
-        self.zks_prior = zks_prior
-        self.n_zks_prior = tf.shape(zks_prior)[1].numpy()
+        self.obs_pos = get_obs_positions(data)
+        self.zks_prior = get_ground_truth_zernike(data)
+        self.n_zks_prior = tf.shape(self.zks_prior)[1].numpy()
+
+        self.n_zks_total = max(
+            model_params.param_hparams.n_zernikes,
+            tf.cast(tf.shape(self.zks_prior)[1], tf.int32),
+        )
+        self.zernike_maps = psfm.generate_zernike_maps_3d(
+            self.n_zks_total, model_params.pupil_diameter
+        )
 
         # Check if the Zernike maps are enough
         if self.n_zks_prior > self.n_zks_total:
@@ -73,18 +195,18 @@ class TFGroundTruthPhysicalField(tf.keras.Model):
         # zernike_maps =[]
 
         # Inputs: TF_batch_poly_PSF
-        self.batch_size = batch_size
-        self.obscurations = obscurations
-        self.output_dim = output_dim
+        self.batch_size = training_params.batch_size
+        self.obscurations = psfm.tf_obscurations(model_params.pupil_diameter)
+        self.output_dim = model_params.output_dim
 
         # Initialize the physical layer
         self.tf_physical_layer = TFPhysicalLayer(
             self.obs_pos,
             self.zks_prior,
-            interpolation_type="none",
+            interpolation_type=None,
         )
         # Initialize the zernike to OPD layer
-        self.tf_zernike_OPD = TFZernikeOPD(zernike_maps=zernike_maps)
+        self.tf_zernike_OPD = TFZernikeOPD(zernike_maps=self.zernike_maps)
 
         # Initialize the batch opd to batch polychromatic PSF layer
         self.tf_batch_poly_PSF = TFBatchPolychromaticPSF(
