e2437ecf08968fc228d3a26204e2b687467790d9
Jennifer Pollack <jennifer.pollack@cea.fr>
2025-03-03 13:43:39 +0100

    Reformat train.py with black

--- src/wf_psf/training/train.py
+++ src/wf_psf/training/train.py
@@ -407,17 +407,17 @@ def train(
             hasattr(psf_model, "save_optim_history_param")
             and psf_model.save_optim_history_param
         ):
-            saving_optim_hist[
-                "param_cycle{}".format(current_cycle)
-            ] = hist_param.history
+            saving_optim_hist["param_cycle{}".format(current_cycle)] = (
+                hist_param.history
+            )
 
         if (
             hasattr(psf_model, "save_optim_history_nonparam")
             and psf_model.save_optim_history_nonparam
         ):
-            saving_optim_hist[
-                "nonparam_cycle{}".format(current_cycle)
-            ] = hist_non_param.history
+            saving_optim_hist["nonparam_cycle{}".format(current_cycle)] = (
+                hist_non_param.history
+            )
 
     # Save last cycle if no cycles were saved
     if not training_handler.multi_cycle_params.save_all_cycles:
