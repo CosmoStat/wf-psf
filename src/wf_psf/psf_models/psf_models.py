2d06e7b0f5afcedafe0c71b505adcf74343d3096
Tobias Liaudat <tobiasliaudat@gmail.com>
2025-03-19 15:07:56 +0100

    add rotation angle param to the tf_obscurations constructor

--- src/wf_psf/psf_models/psf_models.py
+++ src/wf_psf/psf_models/psf_models.py
@@ -247,7 +247,7 @@ def generate_zernike_maps_3d(n_zernikes, pupil_diam):
     return tf.convert_to_tensor(np_zernike_cube, dtype=tf.float32)
 
 
-def tf_obscurations(pupil_diam, N_filter=2):
+def tf_obscurations(pupil_diam, N_filter=2, rot_angle=0):
     """Tensor Flow Obscurations.
 
     A function to generate obscurations as a tensor.
@@ -258,6 +258,8 @@ def tf_obscurations(pupil_diam, N_filter=2):
         Size of the pupil diameter
     N_filters: int
         Number of filters
+    rot_angle: int
+        Rotation angle in degrees to apply to the obscuration pattern. It only supports 90 degree rotations.
 
     Returns
     -------
@@ -266,7 +268,7 @@ def tf_obscurations(pupil_diam, N_filter=2):
 
     """
     obscurations = PSFSimulator.generate_pupil_obscurations(
-        N_pix=pupil_diam, N_filter=N_filter
+        N_pix=pupil_diam, N_filter=N_filter, rot_angle=rot_angle
     )
     return tf.convert_to_tensor(obscurations, dtype=tf.complex64)
 
