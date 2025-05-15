e7b64c9cf7233b27df2ae25e589e9eccc803f8d4
Jennifer Pollack <jennifer.pollack@cea.fr>
2025-05-15 13:21:11 +0200

    Remove arg from evaluate_model unit test

--- src/wf_psf/tests/test_metrics/metrics_interface_test.py
+++ src/wf_psf/tests/test_metrics/metrics_interface_test.py
@@ -76,7 +76,6 @@ def test_evaluate_model(mock_metrics_params, mock_trained_model_params, mock_dat
             trained_model_params=mock_trained_model_params,
             data=mock_data,
             psf_model=mock_psf_model,
-            weights_path="/mock/weights/path",
             metrics_output="/mock/metrics/output"
         )
 
