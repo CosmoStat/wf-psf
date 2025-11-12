615511366481771597ea16fea6459baed983d586
Jennifer Pollack <jennifer.pollack@cea.fr>
2025-11-12 17:41:59 +0100

    Add optimizer handler to build_PSF_model

--- src/wf_psf/psf_models/psf_models.py
+++ src/wf_psf/psf_models/psf_models.py
@@ -11,6 +11,7 @@ import numpy as np
 import tensorflow as tf
 from wf_psf.sims.psf_simulator import PSFSimulator
 from wf_psf.utils.utils import zernike_generator
+from wf_psf.utils.optimizer import get_optimizer
 import glob
 import logging
 
@@ -160,11 +161,11 @@ def build_PSF_model(model_inst, optimizer=None, loss=None, metrics=None):
     if loss is None:
         loss = tf.keras.losses.MeanSquaredError()
 
-    # Define optimizer function
-    if optimizer is None:
-        optimizer = tf.keras.optimizers.legacy.Adam(
-            learning_rate=1e-2, beta_1=0.9, beta_2=0.999, epsilon=1e-07, amsgrad=False
-        )
+    # Handle optimizer: either config object or a Keras optimizer instance
+    if isinstance(optimizer, tf.keras.optimizers.Optimizer):
+        optimizer = optimizer
+    else:
+        optimizer = get_optimizer(optimizer_config=optimizer)
 
     # Define metric functions
     if metrics is None:
