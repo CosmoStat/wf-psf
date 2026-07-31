033a9d212336a144866459fea0b5bf7d4f6661bf
Jennifer Pollack <jennifer.pollack@cea.fr>
2026-07-31 12:10:55 +0200

    Change doc string and QualityMetric class in base.py
    - Improve module doc string to clarify better purpose
    - Replace name property method in QualityMetric to str attribute

--- src/wf_psf/quality_control/metrics/base.py
+++ src/wf_psf/quality_control/metrics/base.py
@@ -3,23 +3,22 @@
 Defines the abstract interface implemented by all quality metrics used
 by the quality control framework.
 
-Quality metrics compute numerical measures describing dataset quality.
+Quality metrics compute numerical measures describing the suitability
+of individual dataset samples for downstream processing, such as model
+training, evaluation, or inference.
 
 :Authors: Jennifer Pollack <jennifer.pollack@cea.fr>
 
 """
+
 from abc import ABC, abstractmethod
+
 
 class QualityMetric(ABC):
     """Abstract interface for quality metric implementations."""
 
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Name of quality metric."""
-        ...
+    name: str
 
     @abstractmethod
     def compute(self, dataset):
         """Compute one metric value for each dataset sample."""
-       
